99bf80539cdfff338b218e45a67ce0a33f205f2b
Konstantin <urkonan79@gmail.com>
2020-07-03 18:23:14 +0300

    Update script.js - Добавил поддежку плохого браузера (IE11)

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
-const tabHeaderItems = document.querySelectorAll('.tab-header__item');
-const marker =document.querySelector('.tab-header__marker ');
-const items = document.querySelectorAll('.tab-content-item');
+"use strict";
 
+const tabHeaderItems = document.querySelectorAll('.tab-header__item');
+const marker = document.querySelector('.tab-header__marker ');
+const items = document.querySelectorAll('.tab-content-item');
 moveMarker(tabHeaderItems[0]);
 
 function moveMarker(t) {
-    marker.style.width = t.clientWidth + 'px';
-    marker.style.left = t.offsetLeft + 'px';
+  marker.style.width = t.clientWidth + 'px';
+  marker.style.left = t.offsetLeft + 'px';
 }
 
 function hideTab() {
-    for(let i = 0; i < items.length; i++) {
-        if(items[i].classList.contains('show')) {
-            items[i].classList.remove('show');
-            break;
-        }
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].classList.contains('show')) {
+      items[i].classList.remove('show');
+      break;
     }
+  }
 }
 
-for(let i = 0; i < tabHeaderItems.length; i++) {
-    tabHeaderItems[i].addEventListener('click', function(event) {
-        moveMarker(event.target);
-        hideTab();
-        items[i].classList.add('show');
-    });
+function toggleTab(i) {
+  tabHeaderItems[i].addEventListener('click', function (event) {
+    moveMarker(event.target);
+    hideTab();
+    console.log(i);
+    items[i].classList.add('show');
+  });
+};
+
+for (let i = 0; i < tabHeaderItems.length; i++) {
+  toggleTab(i);
 }
